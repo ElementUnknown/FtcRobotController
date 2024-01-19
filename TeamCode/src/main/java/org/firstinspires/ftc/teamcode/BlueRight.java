a0d4af5128f04d52006767ba5b839fe6368c46ad
Mason Pade <elementunk@gmail.com>
2024-01-18 19:45:36 -0600

    Tensorflow

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueRight.java
@@ -31,13 +31,32 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.tfod.TfodProcessor;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+
+import java.util.List;
+
 
 @Autonomous(name="BlueRight", group="Robot")
 
 public class BlueRight extends Autonomous_Base {
 
+    private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
+
+    private static final String TFOD_MODEL_ASSET = "balls.tflite";
+    private static final String[] LABELS = {
+            "BlueBall",
+            "RedBall",
+    };
+    private TfodProcessor tfod;
+    private VisionPortal visionPortal;
+
     public void runOpMode() {
 
+        initTfod();
         super.robot.init(super.hardwareMap);
         super.robot.AprilInit(super.hardwareMap);
         if (super.robot.USE_WEBCAM)
@@ -46,6 +65,8 @@ public class BlueRight extends Autonomous_Base {
         waitForStart();
         Move(.8, -23, 0);
         //Check if found in center
+        telemetryTfod();
+        telemetry.update();
         if (checkDistance(10)) {
             PivotTick(4300, 1);
             Move(.8, 7, 0);
@@ -111,5 +132,80 @@ public class BlueRight extends Autonomous_Base {
         Move(.3, -3, 24);
         PivotWaitFinish();//final move to park
         //in all auto codes we should consider changing final position out of the way of the board, maybe to the middle
+        visionPortal.close();
+    }
+
+    private void initTfod() {
+
+        // Create the TensorFlow processor by using a builder.
+        tfod = new TfodProcessor.Builder()
+
+                // Use setModelAssetName() if the TF Model is built in as an asset.
+                // Use setModelFileName() if you have downloaded a custom team model to the Robot Controller.
+                .setModelAssetName(TFOD_MODEL_ASSET)
+                //.setModelFileName(TFOD_MODEL_FILE)
+
+                .setModelLabels(LABELS)
+                //.setIsModelTensorFlow2(true)
+                //.setIsModelQuantized(true)
+                //.setModelInputSize(300)
+                //.setModelAspectRatio(16.0 / 9.0)
+
+                .build();
+
+        // Create the vision portal by using a builder.
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+
+        // Set the camera (webcam vs. built-in RC phone camera).
+        if (USE_WEBCAM) {
+            builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
+        } else {
+            builder.setCamera(BuiltinCameraDirection.BACK);
+        }
+
+        // Choose a camera resolution. Not all cameras support all resolutions.
+        //builder.setCameraResolution(new Size(640, 480));
+
+        // Enable the RC preview (LiveView).  Set "false" to omit camera monitoring.
+        //builder.enableCameraMonitoring(true);
+
+        // Set the stream format; MJPEG uses less bandwidth than default YUY2.
+        //builder.setStreamFormat(VisionPortal.StreamFormat.YUY2);
+
+        // Choose whether or not LiveView stops if no processors are enabled.
+        // If set "true", monitor shows solid orange screen if no processors enabled.
+        // If set "false", monitor shows camera view without annotations.
+        //builder.setAutoStopLiveView(false);
+
+        // Set and enable the processor.
+        builder.addProcessor(tfod);
+
+        // Build the Vision Portal, using the above settings.
+        visionPortal = builder.build();
+
+        // Set confidence threshold for TFOD recognitions, at any time.
+        //tfod.setMinResultConfidence(0.75f);
+
+        // Disable or re-enable the TFOD processor at any time.
+        //visionPortal.setProcessorEnabled(tfod, true);
+
+    }
+
+    private void telemetryTfod() {
+
+        List<Recognition> currentRecognitions = tfod.getRecognitions();
+        telemetry.addData("# Objects Detected", currentRecognitions.size());
+
+        // Step through the list of recognitions and display info for each one.
+        for (Recognition recognition : currentRecognitions) {
+            double x = (recognition.getLeft() + recognition.getRight()) / 2 ;
+            double y = (recognition.getTop()  + recognition.getBottom()) / 2 ;
+
+            telemetry.addData(""," ");
+            telemetry.addData("Image", "%s (%.0f %% Conf.)", recognition.getLabel(), recognition.getConfidence() * 100);
+            telemetry.addData("- Position", "%.0f / %.0f", x, y);
+            telemetry.addData("- Size", "%.0f x %.0f", recognition.getWidth(), recognition.getHeight());
+        }   // end for() loop
+
     }
 }
